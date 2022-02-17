e6326703eb49844ff95857602074d27c5755e60f
Joe Scott <jbs198585@gmail.com>
2022-02-17 15:47:48 -0500

    Methods shorthand

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+// const myMath = {
+//   PI: 3.14159,
+//   square: function (num) {
+//     return num * num;
+//   },
+//   cube: function (num) {
+//     return num ** 3;
+//   },
+// };
+
+// SHORTHAND VERSION
 const myMath = {
   PI: 3.14159,
-  square: function (num) {
+  square(num) {
     return num * num;
   },
-  cube: function (num) {
+  cube(num) {
     return num ** 3;
   },
 };
